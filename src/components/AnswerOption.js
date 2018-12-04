078771d999810de0bb880687489a718830e71e17
Colton Berger <coltonberger@optonline.net>
2018-12-03 17:46:55 -0800

    created quiz with one output

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function AnswerOption(props) {
   return (
-    <li className='anwserOption'>
+    <li className="answerOption">
       <input
-        type='radio'
-        className='radioCustomButton'
-        name='radioGroup'
-        checked={props.answerType === props.anwser}
+        type="radio"
+        className="radioCustomButton"
+        name="radioGroup"
+        checked={props.answerType === props.answer}
         id={props.answerType}
         value={props.answerType}
-        disabled={props.answerType}
+        disabled={props.answer}
         onChange={props.onAnswerSelected}
       />
-      <label className='radioCustomLavel' htmlFor={props.answerType}>
+      <label className="radioCustomLabel" htmlFor={props.answerType}>
         {props.answerContent}
       </label>
     </li>
-  )
+  );
 }
 
 AnswerOption.propTypes = {
   answerType: PropTypes.string.isRequired,
-  anwserContent: PropTypes.string.isRequired,
+  answerContent: PropTypes.string.isRequired,
   answer: PropTypes.string.isRequired,
   onAnswerSelected: PropTypes.func.isRequired
-}
+};
 
-export default AnswerOption
+export default AnswerOption;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,45 +1,52 @@
-import React from 'react'
- import PropTypes from 'prop-types'
- import Question from '../components/Question'
- import QuestionCount from '../components/QuestionCount'
- import AnswerOption from '../components/AnswerOption'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOption from '../components/AnswerOption';
 
- function Quiz(props) {
-     function renderAnswerOptions(key) {
-       return (
-         <AnswerOption
-           key={key.content}
-           answerContent={key.content}
-           answerType={key.type}
-           answer={props.answer}
-           questionId={props.questionId}
-           onAnswerSelected={props.onAnswerSelected}
-         />
-       );
-     }
-
+function Quiz(props) {
+  function renderAnswerOptions(key) {
     return (
-       <div className="quiz">
-         <QuestionCount
-           counter={props.questionId}
-           total={props.questionTotal}
-         />
-         <Question content={props.question} />
-         <ul className="answerOptions">
-           {props.answerOptions.map(renderAnswerOptions)}
-         </ul>
-       </div>
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.answer}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
     );
   }
 
-  Quiz.propTypes = {
-    answer: PropTypes.string.isRequired,
-    answerOptions: PropTypes.array.isRequired,
-    counter: PropTypes.number.isRequired,
-    question: PropTypes.string.isRequired,
-    questionId: PropTypes.number.isRequired,
-    questionTotal: PropTypes.number.isRequired,
-    onAnswerSelected: PropTypes.func.isRequired
-  };
+  return (
+    <CSSTransitionGroup
+      className="container"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div key={props.questionId}>
+        <QuestionCount counter={props.questionId} total={props.questionTotal} />
+        <Question content={props.question} />
+        <ul className="answerOptions">
+          {props.answerOptions.map(renderAnswerOptions)}
+        </ul>
+      </div>
+    </CSSTransitionGroup>
+  );
+}
 
-  export default Quiz;
+Quiz.propTypes = {
+  answer: PropTypes.string.isRequired,
+  answerOptions: PropTypes.array.isRequired,
+  question: PropTypes.string.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTotal: PropTypes.number.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired
+};
+
+export default Quiz;
